e4367d8bd4e5337f1b916a6be7fdedf87dae1560
Anthony Alcorn <xmastony@gmail.com>
2020-08-16 03:12:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,7 @@ def changing_alan
       }
     }
 programmer_hash[:alan_kay][:known_for] = "GUI"
-return programmer_hash[:alan_kay][:known_for]
+return programmer_hash
 end
 
 def adding_to_dennis
